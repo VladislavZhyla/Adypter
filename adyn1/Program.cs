8b230a862aba9895f4d5acced0fa15bea8da0f74
Влад <@DESKTOP-MIKGT6B>
2023-04-11 18:46:32 +0300

    Добавьте файлы проекта.

---
+++ adyn1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adyn1
+{
+   
+    public static class ArraySetConverter
+    {
+        public static HashSet<T> ArrayToSet<T>(T[] array)
+        {
+            return new HashSet<T>(array);
+        }
+
+        public static T[] SetToArray<T>(HashSet<T> set)
+        {
+            return set.ToArray();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 1, 3, 4, 5 };
+            
+            HashSet<int> set = ArraySetConverter.ArrayToSet(array);
+            Console.WriteLine("Set: " + string.Join(", ", set));
+
+            int[] newArray = ArraySetConverter.SetToArray(set);
+            Console.WriteLine("Array: " + string.Join(", ", newArray));
+            Console.ReadKey();
+        }
+    }
+}
+    
